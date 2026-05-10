0a25df46a02bbb2db056ed5ce0f5dd6a12392764
Kurt Romano <kmromano911@gmail.com>
2026-05-10 17:24:17 +0800

    new

---
+++ scripts/slime.gd
@@ -0,0 +1,36 @@
+extends CharacterBody2D
+
+var speed = 50
+var player_chase = false
+var player = null
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+func _ready() -> void:
+	player_chase = false
+	animated_sprite_2d.play("idle_front")
+
+func _physics_process(delta: float) -> void:
+	if player_chase:
+		print("CHASE")
+		position += (player.position - position) / 50
+		$AnimatedSprite2D.play("walk_side")
+		if (player.position.x - position.x) < 0:
+			$AnimatedSprite2D.flip_h = true
+		else:
+			$AnimatedSprite2D.flip_h = false
+	else:
+		print("IDLE")
+		$AnimatedSprite2D.play("idle_front")
+	
+	move_and_slide()
+
+func _on_detection_area_body_entered(body: Node2D) -> void:
+	player = body
+	player_chase = true
+
+
+func _on_detection_area_body_exited(body: Node2D) -> void:
+	player = null
+	player_chase = false
+	
